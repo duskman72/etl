73051f4d424a260baadea45ac7b29c2e4e40535d
Andreas Bratfisch <duskman72@gmail.com>
2022-12-22 05:50:51 +0100

    fix small css issues

--- src/frontend/core/PageHeader.tsx
+++ src/frontend/core/PageHeader.tsx
@@ -4,10 +4,12 @@ import { SearchBox } from "./SearchBox";
  
 export const PageHeader = () => {
     return <header className="page-header">
-        <ThreeBarsIcon className="cursor-pointer text-white ms-2" onClick={() => {
+        <button className="btn btn-menu h-100 ps-2 pe-2 ms-1" onClick={() => {
             const canvas = Offcanvas.getOrCreateInstance(document.querySelector("#offcanvasMenu"));
             canvas.show();
-        }}/>
+        }}>
+            <ThreeBarsIcon className="cursor-pointer text-white" />
+        </button>
         <SearchBox />
     </header>
 }
