90d56470edd74e7435876903143914073d260167
Andreas Bratfisch <duskman72@gmail.com>
2022-12-28 16:40:40 +0100

    make DataSourceType editable

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DataSourceTypesView } from "./pages/DataSourceTypesView";
 import { DataSourcesView } from "./pages/DataSourcesView";
 import { Home } from "./pages/Home";
-
-import "./resources/sass/app.scss"
 import { CredentialsView } from './pages/CredentialsView';
 import { NotFound } from './pages/NotFound';
+import { DataSourceTypesEditView } from './pages/DataSourceTypesEditView';
+
+import "./resources/sass/app.scss"
 
 const App = () => {
     return <BrowserRouter>
         <Routes>
-            <Route path="/data-source-types" element={<DataSourceTypesView /> } />
+            <Route path="/data-source-types/:id" element={<DataSourceTypesEditView /> } />
+            <Route path="/data-source-types" element={<DataSourceTypesView />} />
             <Route path="/data-sources" element={<DataSourcesView /> } />
             <Route path="/credentials" element={<CredentialsView /> } />
             <Route path="/" element={<Home /> } />
---
+++ src/frontend/pages/DataSourceTypesEditView.tsx
@@ -0,0 +1,15 @@
+import { PackageIcon, Page } from "../core"
+
+export const DataSourceTypesEditView = () => {
+    return <Page>
+        <div className="container-fluid">
+            <h5 className="flex align-items-center mb-3">
+                <PackageIcon size={16} className="text-blue-800 me-2" />
+                <span>Edit Data Source Type</span>
+            </h5>
+            <div className="alert alert-danger">
+                NOT IMPLEMENTED.
+            </div>
+        </div>
+    </Page>
+}
